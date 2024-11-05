17c43b685742864fa8a63793f3473766d35787d7
3052 <spen+3052@protonmail.com>
2024-11-05 06:06:20 -0600

    +      Vars.set("MSPR_DEBUG", 0);

--- playReady.go
+++ playReady.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+   "flag"
+   "fmt"
    "os"
    "os/exec"
 )
 
 func main() {
-   os.Setenv("PATH", "C:/Program Files/Android/Android Studio/jbr/bin")
+   err := os.Setenv("PATH", "C:/Program Files/Android/Android Studio/jbr/bin")
+   compile := flag.Bool("c", false, "compile")
+   flag.Parse()
+   if *compile {
+      cmd := exec.Command(
+         "javac", "-d", ".", "*.java", "helpers/*.java", "mod/mspr/*.java",
+      )
+      cmd.Stderr = os.Stderr
+      cmd.Stdout = os.Stdout
+      fmt.Println(cmd.Args)
+      err := cmd.Run()
+      if err != nil {
+         panic(err)
+      }
+   }
+   os.Remove("secrets/genchain")
+   if err != nil {
+      panic(err)
+   }
    data, err := exec.Command("java", "agsecres.tool.Hello").CombinedOutput()
    if err != nil {
       panic(err)
