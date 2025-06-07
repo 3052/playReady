331b6c7af7be12e3a3d592cb4e67ccf888ad1757
367 <367@tuta.io>
2025-06-07 00:20:08 -0500

    +// Envelope represents the root XML structure.

--- xml/xml.go
+++ xml/xml.go
@@ -2,118 +2,185 @@ package xml
 
 import "encoding/xml"
 
+// Envelope represents the root XML structure.
 type Envelope struct {
    XMLName xml.Name `xml:"Envelope"`
    Text    string   `xml:",chardata"`
    Xsi     string   `xml:"xsi,attr"`
    Xsd     string   `xml:"xsd,attr"`
    Soap    string   `xml:"soap,attr"`
-   Body    struct {
-      Text           string `xml:",chardata"`
-      AcquireLicense struct {
-         Text      string `xml:",chardata"`
-         Xmlns     string `xml:"xmlns,attr"`
-         Challenge struct {
-            Text      string `xml:",chardata"`
-            Challenge struct {
-               Text  string `xml:",chardata"`
-               Xmlns string `xml:"xmlns,attr"`
-               LA    struct {
-                  Text          string `xml:",chardata"`
-                  Xmlns         string `xml:"xmlns,attr"`
-                  ID            string `xml:"Id,attr"`
-                  Version       string `xml:"Version"`
-                  ContentHeader struct {
-                     Text      string `xml:",chardata"`
-                     WRMHEADER struct {
-                        Text    string `xml:",chardata"`
-                        Xmlns   string `xml:"xmlns,attr"`
-                        Version string `xml:"version,attr"`
-                        DATA    struct {
-                           Text        string `xml:",chardata"`
-                           PROTECTINFO struct {
-                              Text   string `xml:",chardata"`
-                              KEYLEN string `xml:"KEYLEN"`
-                              ALGID  string `xml:"ALGID"`
-                           } `xml:"PROTECTINFO"`
-                           KID string `xml:"KID"`
-                        } `xml:"DATA"`
-                     } `xml:"WRMHEADER"`
-                  } `xml:"ContentHeader"`
-                  EncryptedData struct {
-                     Text             string `xml:",chardata"`
-                     Xmlns            string `xml:"xmlns,attr"`
-                     Type             string `xml:"Type,attr"`
-                     EncryptionMethod struct {
-                        Text      string `xml:",chardata"`
-                        Algorithm string `xml:"Algorithm,attr"`
-                     } `xml:"EncryptionMethod"`
-                     KeyInfo struct {
-                        Text         string `xml:",chardata"`
-                        Xmlns        string `xml:"xmlns,attr"`
-                        EncryptedKey struct {
-                           Text             string `xml:",chardata"`
-                           Xmlns            string `xml:"xmlns,attr"`
-                           EncryptionMethod struct {
-                              Text      string `xml:",chardata"`
-                              Algorithm string `xml:"Algorithm,attr"`
-                           } `xml:"EncryptionMethod"`
-                           KeyInfo struct {
-                              Text    string `xml:",chardata"`
-                              Xmlns   string `xml:"xmlns,attr"`
-                              KeyName string `xml:"KeyName"`
-                           } `xml:"KeyInfo"`
-                           CipherData struct {
-                              Text        string `xml:",chardata"`
-                              CipherValue string `xml:"CipherValue"`
-                           } `xml:"CipherData"`
-                        } `xml:"EncryptedKey"`
-                     } `xml:"KeyInfo"`
-                     CipherData struct {
-                        Text        string `xml:",chardata"`
-                        CipherValue string `xml:"CipherValue"`
-                     } `xml:"CipherData"`
-                  } `xml:"EncryptedData"`
-               } `xml:"LA"`
-               Signature struct {
-                  Text       string `xml:",chardata"`
-                  Xmlns      string `xml:"xmlns,attr"`
-                  SignedInfo struct {
-                     Text                   string `xml:",chardata"`
-                     Xmlns                  string `xml:"xmlns,attr"`
-                     CanonicalizationMethod struct {
-                        Text      string `xml:",chardata"`
-                        Algorithm string `xml:"Algorithm,attr"`
-                     } `xml:"CanonicalizationMethod"`
-                     SignatureMethod struct {
-                        Text      string `xml:",chardata"`
-                        Algorithm string `xml:"Algorithm,attr"`
-                     } `xml:"SignatureMethod"`
-                     Reference struct {
-                        Text         string `xml:",chardata"`
-                        URI          string `xml:"URI,attr"`
-                        DigestMethod struct {
-                           Text      string `xml:",chardata"`
-                           Algorithm string `xml:"Algorithm,attr"`
-                        } `xml:"DigestMethod"`
-                        DigestValue string `xml:"DigestValue"`
-                     } `xml:"Reference"`
-                  } `xml:"SignedInfo"`
-                  SignatureValue string `xml:"SignatureValue"`
-                  KeyInfo        struct {
-                     Text     string `xml:",chardata"`
-                     Xmlns    string `xml:"xmlns,attr"`
-                     KeyValue struct {
-                        Text        string `xml:",chardata"`
-                        ECCKeyValue struct {
-                           Text      string `xml:",chardata"`
-                           PublicKey string `xml:"PublicKey"`
-                        } `xml:"ECCKeyValue"`
-                     } `xml:"KeyValue"`
-                  } `xml:"KeyInfo"`
-               } `xml:"Signature"`
-            } `xml:"Challenge"`
-         } `xml:"challenge"`
-      } `xml:"AcquireLicense"`
-   } `xml:"Body"`
+   Body    Body     `xml:"Body"`
+}
+
+// Body represents the soap body.
+type Body struct {
+   Text           string         `xml:",chardata"`
+   AcquireLicense AcquireLicense `xml:"AcquireLicense"`
+}
+
+// AcquireLicense represents the AcquireLicense element within the Body.
+type AcquireLicense struct {
+   Text      string    `xml:",chardata"`
+   Xmlns     string    `xml:"xmlns,attr"`
+   Challenge Challenge `xml:"Challenge"`
+}
+
+// Challenge represents the Challenge element within AcquireLicense.
+type Challenge struct {
+   Text      string `xml:",chardata"`
+   Challenge InnerChallenge `xml:"challenge"`
+}
+
+// InnerChallenge represents the inner challenge element.
+type InnerChallenge struct {
+   Text  string `xml:",chardata"`
+   Xmlns string `xml:"xmlns,attr"`
+   LA    LA     `xml:"LA"`
+   Signature Signature `xml:"Signature"`
+}
+
+// LA represents the LA element within the InnerChallenge.
+type LA struct {
+   Text          string        `xml:",chardata"`
+   Xmlns         string        `xml:"xmlns,attr"`
+   ID            string        `xml:"Id,attr"`
+   Version       string        `xml:"Version"`
+   ContentHeader ContentHeader `xml:"ContentHeader"`
+   EncryptedData EncryptedData `xml:"EncryptedData"`
+}
+
+// ContentHeader represents the ContentHeader element within LA.
+type ContentHeader struct {
+   Text    string    `xml:",chardata"`
+   WRMHEADER WRMHEADER `xml:"WRMHEADER"`
+}
+
+// WRMHEADER represents the WRMHEADER element within ContentHeader.
+type WRMHEADER struct {
+   Text    string `xml:",chardata"`
+   Xmlns   string `xml:"xmlns,attr"`
+   Version string `xml:"version,attr"`
+   DATA    DATA   `xml:"DATA"`
+}
+
+// DATA represents the DATA element within WRMHEADER.
+type DATA struct {
+   Text        string      `xml:",chardata"`
+   PROTECTINFO PROTECTINFO `xml:"PROTECTINFO"`
+   KID         string      `xml:"KID"`
+}
+
+// PROTECTINFO represents the PROTECTINFO element within DATA.
+type PROTECTINFO struct {
+   Text   string `xml:",chardata"`
+   KEYLEN string `xml:"KEYLEN"`
+   ALGID  string `xml:"ALGID"`
+}
+
+// EncryptedData represents the EncryptedData element within LA.
+type EncryptedData struct {
+   Text             string           `xml:",chardata"`
+   Xmlns            string           `xml:"xmlns,attr"`
+   Type             string           `xml:"Type,attr"`
+   EncryptionMethod EncryptionMethod `xml:"EncryptionMethod"`
+   KeyInfo          KeyInfo          `xml:"KeyInfo"`
+   CipherData       CipherData       `xml:"CipherData"`
+}
+
+// EncryptionMethod represents the EncryptionMethod element.
+type EncryptionMethod struct {
+   Text      string `xml:",chardata"`
+   Algorithm string `xml:"Algorithm,attr"`
+}
+
+// KeyInfo represents the KeyInfo element within EncryptedData.
+type KeyInfo struct {
+   Text         string       `xml:",chardata"`
+   Xmlns        string       `xml:"xmlns,attr"`
+   EncryptedKey EncryptedKey `xml:"EncryptedKey"`
+}
+
+// EncryptedKey represents the EncryptedKey element within KeyInfo.
+type EncryptedKey struct {
+   Text             string           `xml:",chardata"`
+   Xmlns            string           `xml:"xmlns,attr"`
+   EncryptionMethod EncryptionMethod `xml:"EncryptionMethod"`
+   KeyInfo          InnerKeyInfo     `xml:"KeyInfo"`
+   CipherData       CipherData       `xml:"CipherData"`
+}
+
+// InnerKeyInfo represents the inner KeyInfo element within EncryptedKey.
+type InnerKeyInfo struct {
+   Text    string `xml:",chardata"`
+   Xmlns   string `xml:"xmlns,attr"`
+   KeyName string `xml:"KeyName"`
+}
+
+// CipherData represents the CipherData element.
+type CipherData struct {
+   Text        string `xml:",chardata"`
+   CipherValue string `xml:"CipherValue"`
+}
+
+// Signature represents the Signature element within InnerChallenge.
+type Signature struct {
+   Text         string     `xml:",chardata"`
+   Xmlns        string     `xml:"xmlns,attr"`
+   SignedInfo   SignedInfo `xml:"SignedInfo"`
+   SignatureValue string     `xml:"SignatureValue"`
+   KeyInfo      SignatureKeyInfo `xml:"KeyInfo"`
+}
+
+// SignedInfo represents the SignedInfo element within Signature.
+type SignedInfo struct {
+   Text                   string                 `xml:",chardata"`
+   Xmlns                  string                 `xml:"xmlns,attr"`
+   CanonicalizationMethod CanonicalizationMethod `xml:"CanonicalizationMethod"`
+   SignatureMethod        SignatureMethod        `xml:"SignatureMethod"`
+   Reference              Reference              `xml:"Reference"`
+}
+
+// CanonicalizationMethod represents the CanonicalizationMethod element.
+type CanonicalizationMethod struct {
+   Text      string `xml:",chardata"`
+   Algorithm string `xml:"Algorithm,attr"`
+}
+
+// SignatureMethod represents the SignatureMethod element.
+type SignatureMethod struct {
+   Text      string `xml:",chardata"`
+   Algorithm string `xml:"Algorithm,attr"`
+}
+
+// Reference represents the Reference element within SignedInfo.
+type Reference struct {
+   Text        string       `xml:",chardata"`
+   URI         string       `xml:"URI,attr"`
+   DigestMethod DigestMethod `xml:"DigestMethod"`
+   DigestValue string       `xml:"DigestValue"`
+}
+
+// DigestMethod represents the DigestMethod element within Reference.
+type DigestMethod struct {
+   Text      string `xml:",chardata"`
+   Algorithm string `xml:"Algorithm,attr"`
+}
+
+// SignatureKeyInfo represents the KeyInfo element within Signature.
+type SignatureKeyInfo struct {
+   Text    string   `xml:",chardata"`
+   Xmlns   string   `xml:"xmlns,attr"`
+   KeyValue KeyValue `xml:"KeyValue"`
+}
+
+// KeyValue represents the KeyValue element within SignatureKeyInfo.
+type KeyValue struct {
+   Text        string      `xml:",chardata"`
+   ECCKeyValue ECCKeyValue `xml:"ECCKeyValue"`
+}
+
+// ECCKeyValue represents the ECCKeyValue element within KeyValue.
+type ECCKeyValue struct {
+   Text      string `xml:",chardata"`
+   PublicKey string `xml:"PublicKey"`
 }
