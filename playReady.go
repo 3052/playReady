7d1d211ae1f9858cc62dec1c32000810e1e61f8d
367 <367@tuta.io>
2025-06-08 02:46:58 -0500

    new file:   response.go

--- playReady.go
+++ playReady.go
@@ -2,7 +2,6 @@ package playReady
 
 import (
    "41.neocities.org/playReady/certificate"
-   "41.neocities.org/playReady/license"
    "bytes"
    "crypto/aes"
    "crypto/cipher"
@@ -17,138 +16,12 @@ import (
    "encoding/xml"
    "errors"
    "fmt"
-   "github.com/deatil/go-cryptobin/mac"
    "github.com/deatil/go-cryptobin/mode"
    "math/big"
    "os"
    "slices"
    "strings"
 )
-
-type LicenseResponse struct {
-   RawData          []byte
-   Magic            [4]byte
-   Offset           uint16
-   Version          uint16
-   RightsId         [16]byte
-   OuterContainer   FTLV
-   ContentKeyObject *ContentKey
-   ECCKeyObject     *ECCKey
-   SignatureObject  *license.Signature
-   AuxKeyObject     *AuxKeys
-}
-
-func (l *LicenseResponse) Verify(content_integrity []byte) error {
-   data := l.Encode()
-   data = data[:len(l.RawData)-int(l.SignatureObject.Length)]
-   block, err := aes.NewCipher(content_integrity)
-   if err != nil {
-      return err
-   }
-   data = mac.NewCMAC(block, aes.BlockSize).MAC(data)
-   if !bytes.Equal(data, l.SignatureObject.Data) {
-      return errors.New("failed to decrypt the keys")
-   }
-   return nil
-}
-
-func (l *LicenseResponse) Decode(data []byte) error {
-   l.RawData = make([]byte, len(data))
-   copy(l.RawData, data)
-
-   n := copy(l.Magic[:], data)
-   l.Offset = binary.BigEndian.Uint16(data[n:])
-   n += 2
-   l.Version = binary.BigEndian.Uint16(data[n:])
-   n += 2
-   n += copy(l.RightsId[:], data[n:])
-
-   j, err := l.OuterContainer.Decode(data[n:])
-
-   if err != nil {
-      return err
-   }
-   n += int(j)
-
-   var size uint32
-
-   for size < l.OuterContainer.Length-16 {
-      var ftlv FTLV
-      i, err := ftlv.Decode(l.OuterContainer.Value[int(size):])
-      if err != nil {
-         return err
-      }
-      switch XmrType(ftlv.Type) {
-      case GLOBAL_POLICY_CONTAINER_ENTRY_TYPE: // 2
-         // Rakuten
-      case PLAYBACK_POLICY_CONTAINER_ENTRY_TYPE: // 4
-         // Rakuten
-      case KEY_MATERIAL_CONTAINER_ENTRY_TYPE: // 9
-         var j uint32
-         for j < ftlv.Length-16 {
-            var ftlv2 FTLV
-            k, err := ftlv2.Decode(ftlv.Value[j:])
-            if err != nil {
-               return err
-            }
-            switch XmrType(ftlv2.Type) {
-            case CONTENT_KEY_ENTRY_TYPE: // 10
-               l.ContentKeyObject = new(ContentKey)
-               err = l.ContentKeyObject.Decode(ftlv2.Value)
-               if err != nil {
-                  return err
-               }
-            case DEVICE_KEY_ENTRY_TYPE: // 42
-               l.ECCKeyObject = new(ECCKey)
-               err = l.ECCKeyObject.Decode(ftlv2.Value)
-               if err != nil {
-                  return err
-               }
-            case AUX_KEY_ENTRY_TYPE: // 81
-               l.AuxKeyObject = new(AuxKeys)
-               err = l.AuxKeyObject.Decode(ftlv2.Value)
-               if err != nil {
-                  return err
-               }
-            default:
-               return errors.New("ftlv2.Type")
-            }
-            j += k
-         }
-      case SIGNATURE_ENTRY_TYPE: // 11
-         l.SignatureObject = new(license.Signature)
-         err := l.SignatureObject.Decode(ftlv.Value)
-         l.SignatureObject.Length = uint16(ftlv.Length)
-         if err != nil {
-            return err
-         }
-      default:
-         return errors.New("ftlv.Type")
-      }
-      size += i
-   }
-
-   return nil
-}
-
-func (l *LicenseResponse) Encode() []byte {
-   data := l.Magic[:]
-   data = binary.BigEndian.AppendUint16(data, l.Offset)
-   data = binary.BigEndian.AppendUint16(data, l.Version)
-   data = append(data, l.RightsId[:]...)
-
-   data = append(data, l.OuterContainer.Encode()...)
-   return data
-}
-
-func (l *LicenseResponse) Parse(data string) error {
-   decoded, err := base64.StdEncoding.DecodeString(data)
-
-   if err != nil {
-      return err
-   }
-   return l.Decode(decoded)
-}
 
 type XmrType uint16
 
@@ -377,6 +250,7 @@ func (ld *LocalDevice) ParseLicense(response string) (*KeyData, error) {
       license1.ContentKeyObject.KeyId, license1.ContentKeyObject.Key,
    }, nil
 }
+
 type Config struct {
    Version    string `json:"client_version"`
    CertChain  string `json:"cert_chain"`
---
+++ response_test.go
@@ -0,0 +1,84 @@
+package playReady
+
+import (
+   "bytes"
+   "encoding/hex"
+   "encoding/xml"
+   "io"
+   "net/http"
+   "testing"
+)
+
+var device_test = struct {
+   content string
+   key     string
+   key_id  string
+   url     string
+}{
+   // THIS URL GETS LOCKED TO DEVICE ON FIRST REQUEST
+   url:     "https://prod-playready.rakuten.tv/v1/licensing/pr?uuid=07513603-86ba-41dc-8534-a5156f46cd57",
+   content: "rakuten.tv/cz?content_type=movies&content_id=transvulcania-the-people-s-run",
+   key:     "ab82952e8b567a2359393201e4dde4b4",
+   key_id:  "318f7ece69afcfe3e96de31be6b77272",
+}
+
+const kid = "zn6PMa9p48/pbeMb5rdycg=="
+
+func TestLocal(t *testing.T) {
+   var device LocalDevice
+   device.Version = "2.0.1.3"
+   err := device.CertificateChain.LoadFile(tester.dir + "chain.txt")
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = device.SigningKey.LoadFile(tester.dir + "signing_key.txt")
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = device.EncryptKey.LoadFile(tester.dir + "encrypt_key.txt")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var envelope1 Envelope
+   err = envelope1.New(&device.CertificateChain, device.SigningKey, kid)
+   if err != nil {
+      t.Fatal(err)
+   }
+   data, err := xml.Marshal(envelope1)
+   if err != nil {
+      t.Fatal(err)
+   }
+   resp, err := http.Post(device_test.url, "", bytes.NewReader(data))
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer resp.Body.Close()
+   data1, err := io.ReadAll(resp.Body)
+   if err != nil {
+      t.Fatal(err)
+   }
+   if resp.StatusCode != http.StatusOK {
+      var envelope struct {
+         Body struct {
+            Fault struct {
+               Fault string `xml:"faultstring"`
+            }
+         }
+      }
+      err = xml.Unmarshal(data1, &envelope)
+      if err != nil {
+         t.Fatal(err)
+      }
+      t.Fatal(envelope)
+   }
+   key, err := device.ParseLicense(string(data1))
+   if err != nil {
+      t.Fatal(err)
+   }
+   if hex.EncodeToString(key.KeyId.Encode()) != device_test.key_id {
+      t.Fatal(".KeyId")
+   }
+   if hex.EncodeToString(key.Key.Bytes()) != device_test.key {
+      t.Fatal(".Key")
+   }
+}
