841d952df8390eb13c4f73bb627684683873a091
367 <367@tuta.io>
2025-06-12 21:26:02 -0500

    deleted:    chain_test.go

--- device_test.go
+++ device_test.go
@@ -6,11 +6,61 @@ import (
    "encoding/hex"
    "encoding/xml"
    "io"
+   "log"
    "net/http"
    "os"
    "testing"
 )
 
+func TestChain(t *testing.T) {
+   data, err := os.ReadFile(SL2000.dir + SL2000.g1)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var chain1 Chain
+   err = chain1.Decode(data)
+   if err != nil {
+      t.Fatal(err)
+   }
+   data, err = os.ReadFile(SL2000.dir + SL2000.z1)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var z1 EcKey
+   z1.LoadBytes(data)
+   // they downgrade certs from the cert digest (hash of the signing key)
+   var signing_key EcKey
+   err = signing_key.New()
+   if err != nil {
+      t.Fatal(err)
+   }
+   var encrypt_key EcKey
+   err = encrypt_key.New()
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = chain1.CreateLeaf(z1, signing_key, encrypt_key)
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = write_file(SL2000.dir+"chain.txt", chain1.Encode())
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = write_file(SL2000.dir+"signing_key.txt", signing_key.Private())
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = write_file(SL2000.dir+"encrypt_key.txt", encrypt_key.Private())
+   if err != nil {
+      t.Fatal(err)
+   }
+}
+
+func write_file(name string, data []byte) error {
+   log.Println("WriteFile", name)
+   return os.WriteFile(name, data, os.ModePerm)
+}
 var rakuten = struct {
    content string
    key     string
