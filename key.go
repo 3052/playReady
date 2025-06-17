eccb3a69113e5cb9e2395b36792bb0327ca8fb31
367 <367@tuta.io>
2025-06-16 23:47:11 -0500

    +func (f *features) decode(data []byte) int {

--- key.go
+++ key.go
@@ -11,6 +11,90 @@ import (
    "slices"
 )
 
+func (f *features) decode(data []byte) int {
+   f.entries = binary.BigEndian.Uint32(data)
+   n := 4
+   for range f.entries {
+      f.features = append(f.features, binary.BigEndian.Uint32(data[n:]))
+      n += 4
+   }
+   return n
+}
+
+// new initializes a new key with provided data and type.
+func (k *keyData) New(data []byte, Type int) {
+   k.keyType = 1  // Assuming type 1 is for ECDSA keys
+   k.length = 512 // Assuming key length in bits
+   copy(k.publicKey[:], data)
+   k.usage.New(Type)
+}
+
+type keyData struct {
+   keyType   uint16
+   length    uint16
+   flags     uint32
+   // ECDSA P256 public key is 64 bytes (X and Y coordinates, 32 bytes each)
+   publicKey [64]byte
+   // Features indicating key usage
+   usage     features
+}
+
+// encode encodes the key structure into a byte slice.
+func (k *keyData) encode() []byte {
+   data := binary.BigEndian.AppendUint16(nil, k.keyType)
+   data = binary.BigEndian.AppendUint16(data, k.length)
+   data = binary.BigEndian.AppendUint32(data, k.flags)
+   data = append(data, k.publicKey[:]...)
+   return append(data, k.usage.encode()...)
+}
+
+// new initializes a new keyInfo with signing and encryption keys.
+func (k *keyInfo) New(signingKey, encryptKey []byte) {
+   k.entries = 2
+   k.keys = make([]keyData, 2)
+   k.keys[0].New(signingKey, 1) // Type 1 for signing key
+   k.keys[1].New(encryptKey, 2) // Type 2 for encryption key
+}
+
+// xorKey performs XOR operation on two byte slices.
+func xorKey(a, b []byte) []byte {
+   if len(a) != len(b) {
+      panic("slices have different lengths")
+   }
+   c := make([]byte, len(a))
+   for i := 0; i < len(a); i++ {
+      c[i] = a[i] ^ b[i]
+   }
+   return c
+}
+
+// magicConstantZero returns a specific hex-decoded byte slice.
+func (*ContentKey) magicConstantZero() ([]byte, error) {
+   return hex.DecodeString("7ee9ed4af773224f00b8ea7efb027cbb")
+}
+
+// Decode decodes a byte slice into an ECCKey structure.
+func (e *eccKey) decode(data []byte) {
+   e.Curve = binary.BigEndian.Uint16(data)
+   data = data[2:]
+   e.Length = binary.BigEndian.Uint16(data)
+   data = data[2:]
+   e.Value = data
+}
+
+// decode decodes a byte slice into a ContentKey structure.
+func (c *ContentKey) decode(data []byte) {
+   c.KeyID.Decode(data[:])
+   data = data[16:]
+   c.KeyType = binary.BigEndian.Uint16(data)
+   data = data[2:]
+   c.CipherType = binary.BigEndian.Uint16(data)
+   data = data[2:]
+   c.Length = binary.BigEndian.Uint16(data)
+   data = data[2:]
+   c.Value = data
+}
+
 func newLa(m *ecdsa.PublicKey, cipherData []byte, kid string) xml.La {
    return xml.La{
       XmlNs:   "http://schemas.microsoft.com/DRM/2007/03/protocols",
@@ -110,51 +194,6 @@ func (c *ContentKey) decrypt(key *ecdsa.PrivateKey, auxKeys *auxKeys) error {
    return errors.New("cannot decrypt key")
 }
 
-func (c *ContentKey) scalable(key *ecdsa.PrivateKey, auxKeys *auxKeys) error {
-   rootKeyInfo := c.Value[:144]
-   rootKey := rootKeyInfo[128:]
-   leafKeys := c.Value[144:]
-   decrypted := elGamalDecrypt(rootKeyInfo[:128], key)
-   var (
-      ci [16]byte
-      ck [16]byte
-   )
-   for i := range 16 {
-      ci[i] = decrypted[i*2]
-      ck[i] = decrypted[i*2+1]
-   }
-   magicConstantZero, err := c.magicConstantZero()
-   if err != nil {
-      return err
-   }
-   rgbUplinkXkey := xorKey(ck[:], magicConstantZero)
-   contentKeyPrime, err := aesECBHandler(rgbUplinkXkey, ck[:], true)
-   if err != nil {
-      return err
-   }
-   auxKeyCalc, err := aesECBHandler(auxKeys.Keys[0].Key[:], contentKeyPrime, true)
-   if err != nil {
-      return err
-   }
-   var zero [16]byte
-   upLinkXkey := xorKey(auxKeyCalc, zero[:])
-   oSecondaryKey, err := aesECBHandler(rootKey, ck[:], true)
-   if err != nil {
-      return err
-   }
-   rgbKey, err := aesECBHandler(leafKeys, upLinkXkey, true)
-   if err != nil {
-      return err
-   }
-   rgbKey, err = aesECBHandler(rgbKey, oSecondaryKey, true)
-   if err != nil {
-      return err
-   }
-   c.Integrity.Decode(rgbKey[:])
-   rgbKey = rgbKey[16:]
-   copy(c.Key[:], rgbKey)
-   return nil
-}
 // they downgrade certs from the cert digest (hash of the signing key)
 func (f Fill) key() (*EcKey, error) {
    key, err := ecdsa.GenerateKey(elliptic.P256(), f)
@@ -261,77 +300,7 @@ func (k *keyData) decode(data []byte) int {
    return n
 }
 
-func (f *features) decode(data []byte) int {
-   f.entries = binary.BigEndian.Uint32(data)
-   n := 4
-   for range f.entries {
-      f.features = append(f.features, binary.BigEndian.Uint32(data[n:]))
-      n += 4
-   }
-   return n
-}
-
-type keyData struct {
-   keyType   uint16
-   length    uint16
-   flags     uint32
-   // ECDSA P256 public key is 64 bytes (X and Y coordinates, 32 bytes each)
-   publicKey [64]byte
-   // Features indicating key usage
-   usage     features
-}
-
-// new initializes a new key with provided data and type.
-func (k *keyData) New(data []byte, Type int) {
-   k.keyType = 1  // Assuming type 1 is for ECDSA keys
-   k.length = 512 // Assuming key length in bits
-   copy(k.publicKey[:], data)
-   k.usage.New(Type)
-}
-
-// encode encodes the key structure into a byte slice.
-func (k *keyData) encode() []byte {
-   data := binary.BigEndian.AppendUint16(nil, k.keyType)
-   data = binary.BigEndian.AppendUint16(data, k.length)
-   data = binary.BigEndian.AppendUint32(data, k.flags)
-   data = append(data, k.publicKey[:]...)
-   return append(data, k.usage.encode()...)
-}
-
-// new initializes a new keyInfo with signing and encryption keys.
-func (k *keyInfo) New(signingKey, encryptKey []byte) {
-   k.entries = 2
-   k.keys = make([]keyData, 2)
-   k.keys[0].New(signingKey, 1) // Type 1 for signing key
-   k.keys[1].New(encryptKey, 2) // Type 2 for encryption key
-}
-
-// xorKey performs XOR operation on two byte slices.
-func xorKey(a, b []byte) []byte {
-   if len(a) != len(b) {
-      panic("slices have different lengths")
-   }
-   c := make([]byte, len(a))
-   for i := 0; i < len(a); i++ {
-      c[i] = a[i] ^ b[i]
-   }
-   return c
-}
-
-// magicConstantZero returns a specific hex-decoded byte slice.
-func (*ContentKey) magicConstantZero() ([]byte, error) {
-   return hex.DecodeString("7ee9ed4af773224f00b8ea7efb027cbb")
-}
-
-func (f *features) encode() []byte {
-   data := binary.BigEndian.AppendUint32(nil, f.entries)
-
-   for i := range f.entries {
-      data = binary.BigEndian.AppendUint32(data, f.features[i])
-   }
-
-   return data
-}
+///
 
 // encode encodes the keyInfo structure into a byte slice.
 func (k *keyInfo) encode() []byte {
@@ -344,24 +313,58 @@ func (k *keyInfo) encode() []byte {
    return data
 }
 
-// Decode decodes a byte slice into an ECCKey structure.
-func (e *eccKey) decode(data []byte) {
-   e.Curve = binary.BigEndian.Uint16(data)
-   data = data[2:]
-   e.Length = binary.BigEndian.Uint16(data)
-   data = data[2:]
-   e.Value = data
+func (f *features) encode() []byte {
+   data := binary.BigEndian.AppendUint32(nil, f.entries)
+
+   for i := range f.entries {
+      data = binary.BigEndian.AppendUint32(data, f.features[i])
+   }
+
+   return data
 }
 
-// decode decodes a byte slice into a ContentKey structure.
-func (c *ContentKey) decode(data []byte) {
-   c.KeyID.Decode(data[:])
-   data = data[16:]
-   c.KeyType = binary.BigEndian.Uint16(data)
-   data = data[2:]
-   c.CipherType = binary.BigEndian.Uint16(data)
-   data = data[2:]
-   c.Length = binary.BigEndian.Uint16(data)
-   data = data[2:]
-   c.Value = data
+func (c *ContentKey) scalable(key *ecdsa.PrivateKey, auxKeys *auxKeys) error {
+   rootKeyInfo := c.Value[:144]
+   rootKey := rootKeyInfo[128:]
+   leafKeys := c.Value[144:]
+   decrypted := elGamalDecrypt(rootKeyInfo[:128], key)
+   var (
+      ci [16]byte
+      ck [16]byte
+   )
+   for i := range 16 {
+      ci[i] = decrypted[i*2]
+      ck[i] = decrypted[i*2+1]
+   }
+   magicConstantZero, err := c.magicConstantZero()
+   if err != nil {
+      return err
+   }
+   rgbUplinkXkey := xorKey(ck[:], magicConstantZero)
+   contentKeyPrime, err := aesECBHandler(rgbUplinkXkey, ck[:], true)
+   if err != nil {
+      return err
+   }
+   auxKeyCalc, err := aesECBHandler(auxKeys.Keys[0].Key[:], contentKeyPrime, true)
+   if err != nil {
+      return err
+   }
+   var zero [16]byte
+   upLinkXkey := xorKey(auxKeyCalc, zero[:])
+   oSecondaryKey, err := aesECBHandler(rootKey, ck[:], true)
+   if err != nil {
+      return err
+   }
+   rgbKey, err := aesECBHandler(leafKeys, upLinkXkey, true)
+   if err != nil {
+      return err
+   }
+   rgbKey, err = aesECBHandler(rgbKey, oSecondaryKey, true)
+   if err != nil {
+      return err
+   }
+   c.Integrity.Decode(rgbKey[:])
+   rgbKey = rgbKey[16:]
+   copy(c.Key[:], rgbKey)
+   return nil
 }
