9d3ecac6804e8013f2620c31518659a817ed987c
367 <367@tuta.io>
2025-06-18 22:40:48 -0500

    +func (c *Chain) cipherData(key *xmlKey) ([]byte, error) {

--- chain.go
+++ chain.go
@@ -12,226 +12,6 @@ import (
    "slices"
 )
 
-func (c *Certificate) decode(data []byte) (int, error) {
-   n := copy(c.magic[:], data)
-   if string(c.magic[:]) != "CERT" {
-      return 0, errors.New("failed to find cert magic")
-   }
-   c.version = binary.BigEndian.Uint32(data[n:])
-   n += 4
-   c.length = binary.BigEndian.Uint32(data[n:])
-   n += 4
-   c.lengthToSignature = binary.BigEndian.Uint32(data[n:])
-   n += 4
-   c.rawData = data[n:][:c.length-16]
-   n += len(c.rawData)
-   var n1 int
-   for n1 < len(c.rawData) {
-      var value ftlv
-      n1 += value.decode(c.rawData[n1:])
-      switch value.Type {
-      case objTypeBasic:
-         c.certificateInfo = &certificateInfo{}
-         c.certificateInfo.decode(value.Value)
-      case objTypeFeature:
-         c.features = &features{}
-         c.features.decode(value.Value)
-      case objTypeKey:
-         c.keyInfo = &keyInfo{}
-         c.keyInfo.decode(value.Value)
-      case objTypeManufacturer:
-         c.manufacturerInfo = &manufacturer{}
-         c.manufacturerInfo.decode(value.Value)
-      case objTypeSignature:
-         c.signature = &certificateSignature{}
-         c.signature.decode(value.Value)
-      }
-   }
-   return n, nil
-}
-
-func (c *Certificate) verify(pubKey []byte) bool {
-   if !bytes.Equal(c.signature.IssuerKey, pubKey) {
-      return false
-   }
-   // Reconstruct the ECDSA public key from the byte slice.
-   publicKey := ecdsa.PublicKey{
-      Curve: elliptic.P256(), // Assuming P256 curve
-      X:     new(big.Int).SetBytes(pubKey[:32]),
-      Y:     new(big.Int).SetBytes(pubKey[32:]),
-   }
-   data := c.encode()
-   data = data[:c.lengthToSignature]
-   signatureDigest := sha256.Sum256(data)
-   signature := c.signature.SignatureData
-   r := new(big.Int).SetBytes(signature[:32])
-   s := new(big.Int).SetBytes(signature[32:])
-   return ecdsa.Verify(&publicKey, signatureDigest[:], r, s)
-}
-
-type Certificate struct {
-   magic             [4]byte
-   version           uint32
-   length            uint32
-   lengthToSignature uint32
-   rawData           []byte
-   certificateInfo   *certificateInfo
-   features          *features
-   keyInfo           *keyInfo
-   manufacturerInfo  *manufacturer
-   signature         *certificateSignature
-}
-
-func (c *Certificate) newNoSig(data []byte) {
-   copy(c.magic[:], "CERT")
-   c.version = 1
-   // length = length of raw data + header size (16) + signature size (144)
-   c.length = uint32(len(data)) + 16 + 144
-   // lengthToSignature = length of raw data + header size (16)
-   c.lengthToSignature = uint32(len(data)) + 16
-   c.rawData = data
-}
-
-// encode encodes the Cert structure into a byte slice.
-func (c *Certificate) encode() []byte {
-   data := c.magic[:]
-   data = binary.BigEndian.AppendUint32(data, c.version)
-   data = binary.BigEndian.AppendUint32(data, c.length)
-   data = binary.BigEndian.AppendUint32(data, c.lengthToSignature)
-   return append(data, c.rawData...)
-}
-
-type Chain struct {
-   Magic     [4]byte
-   Version   uint32
-   Length    uint32
-   Flags     uint32
-   CertCount uint32
-   Certs     []Certificate
-}
-
-func (c *Chain) RequestBody(signEncrypt EcKey, kid []byte) ([]byte, error) {
-   var key xmlKey
-   key.New()
-   cipherData, err := c.cipherData(&key)
-   if err != nil {
-      return nil, err
-   }
-   la := newLa(&key.PublicKey, cipherData, kid)
-   laData, err := la.Marshal()
-   if err != nil {
-      return nil, err
-   }
-   laDigest := sha256.Sum256(laData)
-   signedInfo := xml.SignedInfo{
-      XmlNs: "http://www.w3.org/2000/09/xmldsig#",
-      Reference: xml.Reference{
-         Uri:         "#SignedData",
-         DigestValue: laDigest[:],
-      },
-   }
-   signedData, err := signedInfo.Marshal()
-   if err != nil {
-      return nil, err
-   }
-   signedDigest := sha256.Sum256(signedData)
-   r, s, err := ecdsa.Sign(Fill('B'), signEncrypt[0], signedDigest[:])
-   if err != nil {
-      return nil, err
-   }
-   envelope := xml.Envelope{
-      Soap: "http://schemas.xmlsoap.org/soap/envelope/",
-      Body: xml.Body{
-         AcquireLicense: &xml.AcquireLicense{
-            XmlNs: "http://schemas.microsoft.com/DRM/2007/03/protocols",
-            Challenge: xml.Challenge{
-               Challenge: xml.InnerChallenge{
-                  XmlNs: "http://schemas.microsoft.com/DRM/2007/03/protocols/messages",
-                  La:    la,
-                  Signature: xml.Signature{
-                     SignedInfo:     signedInfo,
-                     SignatureValue: append(r.Bytes(), s.Bytes()...),
-                  },
-               },
-            },
-         },
-      },
-   }
-   return envelope.Marshal()
-}
-
-func (c *Chain) Leaf(modelKey, signEncryptKey *EcKey) error {
-   if !bytes.Equal(c.Certs[0].keyInfo.keys[0].publicKey[:], modelKey.Public()) {
-      return errors.New("zgpriv not for cert")
-   }
-   // Verify the existing chain's validity.
-   if !c.verify() {
-      return errors.New("cert is not valid")
-   }
-   var leafData bytes.Buffer
-   {
-      digest := sha256.Sum256(signEncryptKey.Public())
-      var data certificateInfo
-      data.New(c.Certs[0].certificateInfo.securityLevel, digest[:])
-      var value ftlv
-      value.New(1, 1, data.encode())
-      leafData.Write(value.encode())
-   }
-   {
-      // Create a new device and its FTLV.
-      var data device
-      data.New()
-      var value ftlv
-      value.New(1, 4, data.encode())
-      leafData.Write(value.encode())
-   }
-   {
-      // SCALABLE with SL2000, SUPPORTS_PR3_FEATURES
-      data := features{
-         entries:  1,
-         features: []uint32{0xD},
-      }
-      // Create FTLV for features.
-      var value ftlv
-      value.New(1, 5, data.encode())
-      leafData.Write(value.encode())
-   }
-   {
-      var data keyInfo
-      data.New(signEncryptKey.Public())
-      var value ftlv
-      value.New(1, 6, data.encode())
-      leafData.Write(value.encode())
-   }
-   {
-      // Create FTLV for manufacturer information, copying from the existing
-      // chain's first cert.
-      var value ftlv
-      value.New(0, 7, c.Certs[0].manufacturerInfo.encode())
-      leafData.Write(value.encode())
-   }
-   var unsigned Certificate
-   unsigned.newNoSig(leafData.Bytes())
-   {
-      digest := sha256.Sum256(unsigned.encode())
-      r, s, err := ecdsa.Sign(Fill('A'), modelKey[0], digest[:])
-      if err != nil {
-         return err
-      }
-      var data certificateSignature
-      data.New(append(r.Bytes(), s.Bytes()...), modelKey.Public())
-      var value ftlv
-      value.New(1, 8, data.encode())
-      leafData.Write(value.encode())
-   }
-   unsigned.length = uint32(leafData.Len()) + 16
-   unsigned.rawData = leafData.Bytes()
-   c.Length += unsigned.length
-   c.CertCount += 1
-   c.Certs = slices.Insert(c.Certs, 0, unsigned)
-   return nil
-}
-
 func (c *Chain) cipherData(key *xmlKey) ([]byte, error) {
    data := xml.Data{
       CertificateChains: xml.CertificateChains{
@@ -305,3 +85,224 @@ func (c *Chain) Decode(data []byte) error {
    return nil
 }
 
+type Chain struct {
+   Magic     [4]byte
+   Version   uint32
+   Length    uint32
+   Flags     uint32
+   CertCount uint32
+   Certs     []Certificate
+}
+
+func (c *Chain) RequestBody(signEncrypt EcKey, kid []byte) ([]byte, error) {
+   var key xmlKey
+   key.New()
+   cipherData, err := c.cipherData(&key)
+   if err != nil {
+      return nil, err
+   }
+   la := newLa(&key.PublicKey, cipherData, kid)
+   laData, err := la.Marshal()
+   if err != nil {
+      return nil, err
+   }
+   laDigest := sha256.Sum256(laData)
+   signedInfo := xml.SignedInfo{
+      XmlNs: "http://www.w3.org/2000/09/xmldsig#",
+      Reference: xml.Reference{
+         Uri:         "#SignedData",
+         DigestValue: laDigest[:],
+      },
+   }
+   signedData, err := signedInfo.Marshal()
+   if err != nil {
+      return nil, err
+   }
+   signedDigest := sha256.Sum256(signedData)
+   r, s, err := ecdsa.Sign(Fill('B'), signEncrypt[0], signedDigest[:])
+   if err != nil {
+      return nil, err
+   }
+   envelope := xml.Envelope{
+      Soap: "http://schemas.xmlsoap.org/soap/envelope/",
+      Body: xml.Body{
+         AcquireLicense: &xml.AcquireLicense{
+            XmlNs: "http://schemas.microsoft.com/DRM/2007/03/protocols",
+            Challenge: xml.Challenge{
+               Challenge: xml.InnerChallenge{
+                  XmlNs: "http://schemas.microsoft.com/DRM/2007/03/protocols/messages",
+                  La:    la,
+                  Signature: xml.Signature{
+                     SignedInfo:     signedInfo,
+                     SignatureValue: append(r.Bytes(), s.Bytes()...),
+                  },
+               },
+            },
+         },
+      },
+   }
+   return envelope.Marshal()
+}
+
+func (c *Certificate) verify(pubKey []byte) bool {
+   if !bytes.Equal(c.signature.IssuerKey, pubKey) {
+      return false
+   }
+   // Reconstruct the ECDSA public key from the byte slice.
+   publicKey := ecdsa.PublicKey{
+      Curve: elliptic.P256(), // Assuming P256 curve
+      X:     new(big.Int).SetBytes(pubKey[:32]),
+      Y:     new(big.Int).SetBytes(pubKey[32:]),
+   }
+   data := c.encode()
+   data = data[:c.LengthToSignature]
+   signatureDigest := sha256.Sum256(data)
+   signature := c.signature.SignatureData
+   r := new(big.Int).SetBytes(signature[:32])
+   s := new(big.Int).SetBytes(signature[32:])
+   return ecdsa.Verify(&publicKey, signatureDigest[:], r, s)
+}
+
+///
+
+func (c *Certificate) newNoSig(data []byte) {
+   copy(c.Magic[:], "CERT")
+   c.Version = 1
+   // length = length of raw data + header size (16) + signature size (144)
+   c.Length = uint32(len(data)) + 16 + 144
+   // lengthToSignature = length of raw data + header size (16)
+   c.LengthToSignature = uint32(len(data)) + 16
+   c.rawData = data
+}
+
+type Certificate struct {
+   Magic             [4]byte
+   Version           uint32
+   Length            uint32
+   LengthToSignature uint32
+   rawData           []byte
+   certificateInfo   *certificateInfo
+   features          *features
+   keyInfo           *keyInfo
+   manufacturerInfo  *manufacturer
+   signature         *certificateSignature
+}
+
+func (c *Certificate) decode(data []byte) (int, error) {
+   n := copy(c.Magic[:], data)
+   if string(c.Magic[:]) != "CERT" {
+      return 0, errors.New("failed to find cert magic")
+   }
+   c.Version = binary.BigEndian.Uint32(data[n:])
+   n += 4
+   c.Length = binary.BigEndian.Uint32(data[n:])
+   n += 4
+   c.LengthToSignature = binary.BigEndian.Uint32(data[n:])
+   n += 4
+   c.rawData = data[n:][:c.Length-16]
+   n += len(c.rawData)
+   var n1 int
+   for n1 < len(c.rawData) {
+      var value ftlv
+      n1 += value.decode(c.rawData[n1:])
+      switch value.Type {
+      case objTypeBasic:
+         c.certificateInfo = &certificateInfo{}
+         c.certificateInfo.decode(value.Value)
+      case objTypeFeature:
+         c.features = &features{}
+         c.features.decode(value.Value)
+      case objTypeKey:
+         c.keyInfo = &keyInfo{}
+         c.keyInfo.decode(value.Value)
+      case objTypeManufacturer:
+         c.manufacturerInfo = &manufacturer{}
+         c.manufacturerInfo.decode(value.Value)
+      case objTypeSignature:
+         c.signature = &certificateSignature{}
+         c.signature.decode(value.Value)
+      }
+   }
+   return n, nil
+}
+
+// encode encodes the Cert structure into a byte slice.
+func (c *Certificate) encode() []byte {
+   data := c.Magic[:]
+   data = binary.BigEndian.AppendUint32(data, c.Version)
+   data = binary.BigEndian.AppendUint32(data, c.Length)
+   data = binary.BigEndian.AppendUint32(data, c.LengthToSignature)
+   return append(data, c.rawData...)
+}
+
+func (c *Chain) Leaf(modelKey, signEncryptKey *EcKey) error {
+   if !bytes.Equal(c.Certs[0].keyInfo.keys[0].publicKey[:], modelKey.Public()) {
+      return errors.New("zgpriv not for cert")
+   }
+   // Verify the existing chain's validity.
+   if !c.verify() {
+      return errors.New("cert is not valid")
+   }
+   var leafData bytes.Buffer
+   {
+      digest := sha256.Sum256(signEncryptKey.Public())
+      var data certificateInfo
+      data.New(c.Certs[0].certificateInfo.securityLevel, digest[:])
+      var value ftlv
+      value.New(1, 1, data.encode())
+      leafData.Write(value.encode())
+   }
+   {
+      // Create a new device and its FTLV.
+      var data device
+      data.New()
+      var value ftlv
+      value.New(1, 4, data.encode())
+      leafData.Write(value.encode())
+   }
+   {
+      // SCALABLE with SL2000, SUPPORTS_PR3_FEATURES
+      data := features{
+         entries:  1,
+         features: []uint32{0xD},
+      }
+      // Create FTLV for features.
+      var value ftlv
+      value.New(1, 5, data.encode())
+      leafData.Write(value.encode())
+   }
+   {
+      var data keyInfo
+      data.New(signEncryptKey.Public())
+      var value ftlv
+      value.New(1, 6, data.encode())
+      leafData.Write(value.encode())
+   }
+   {
+      // Create FTLV for manufacturer information, copying from the existing
+      // chain's first cert.
+      var value ftlv
+      value.New(0, 7, c.Certs[0].manufacturerInfo.encode())
+      leafData.Write(value.encode())
+   }
+   var unsigned Certificate
+   unsigned.newNoSig(leafData.Bytes())
+   {
+      digest := sha256.Sum256(unsigned.encode())
+      r, s, err := ecdsa.Sign(Fill('A'), modelKey[0], digest[:])
+      if err != nil {
+         return err
+      }
+      var data certificateSignature
+      data.New(append(r.Bytes(), s.Bytes()...), modelKey.Public())
+      var value ftlv
+      value.New(1, 8, data.encode())
+      leafData.Write(value.encode())
+   }
+   unsigned.Length = uint32(leafData.Len()) + 16
+   unsigned.rawData = leafData.Bytes()
+   c.Length += unsigned.Length
+   c.CertCount += 1
+   c.Certs = slices.Insert(c.Certs, 0, unsigned)
+   return nil
+}
