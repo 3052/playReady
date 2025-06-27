76d6eccf15fcbf97d4f94b8d46100dae3e2f5dff
367 <367@tuta.io>
2025-06-27 18:28:45 -0500

    new file:   internal/2025-06-26/elGamal.go

---
+++ internal/2025-06-26/elGamal.go
@@ -0,0 +1,17 @@
+package elGamal
+
+var Modules = []struct{
+   note string
+   url string
+}{
+   {
+      url: "github.com/golang/go/blob/master/src/crypto/internal/fips140/nistec/p256.go",
+   },
+   {
+      note: "export internal/sm2ec",
+      url: "github.com/emmansun/gmsm/issues/339",
+   },
+   {
+      url: "pkg.go.dev/filippo.io/nistec",
+   },
+}
