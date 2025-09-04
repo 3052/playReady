5869fa5184b847a74c22561e42332aefde4c9a88
367 <367@tuta.io>
2025-09-04 16:42:35 -0500

    +func p256() *curve {

--- chain.go
+++ chain.go
@@ -34,17 +34,6 @@ func sign(hashVal []byte, privK *big.Int) ([]byte, error) {
    return append(rs[0].Bytes(), rs[1].Bytes()...), nil
 }
 
-// nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-4.pdf
-func p256() *curve {
-   var c curve
-   c.EC.A = big.NewInt(-3)
-   c.EC.Q, _ = new(big.Int).SetString("115792089210356248762697446949407573530086143415290314195533631308867097853951", 10)
-   c.G.X, _ = new(big.Int).SetString("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16)
-   c.G.Y, _ = new(big.Int).SetString("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16)
-   c.N, _ = new(big.Int).SetString("115792089210356248762697446949407573529996955224135760342422259061068512044369", 10)
-   return &c
-}
-
 func elGamalDecrypt(data []byte, privK *big.Int) ([]byte, error) {
    // Unmarshal C1 component
    c1 := ecc.Point{
@@ -62,6 +51,19 @@ func elGamalDecrypt(data []byte, privK *big.Int) ([]byte, error) {
    }
    return append(point.X.Bytes(), point.Y.Bytes()...), nil
 }
+
+// nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-4.pdf
+func p256() *curve {
+   var c curve
+   c.EC.A = big.NewInt(-3)
+   c.EC.Q, _ = new(big.Int).SetString("115792089210356248762697446949407573530086143415290314195533631308867097853951", 10)
+   c.G.X, _ = new(big.Int).SetString("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16)
+   c.G.Y, _ = new(big.Int).SetString("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16)
+   c.N, _ = new(big.Int).SetString("115792089210356248762697446949407573529996955224135760342422259061068512044369", 10)
+   return &c
+}
+
+///
 
 func elGamalEncrypt(m, pubK *ecc.Point) ([]byte, error) {
    c, err := p256().eg().Encrypt(*m, *pubK, big.NewInt(1))
